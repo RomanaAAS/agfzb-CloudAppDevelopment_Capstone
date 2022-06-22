6e607ac938bf2901f5896ca86c79f1ace6de8079
RomanaAAS <romana.leitgeb247@gmail.com>
2022-06-22 18:44:02 +0000

    repaired the deployment

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost", "localhost:8000","rl_djangoapp.mybluemix.net", "https://romanaleitge-8000.theiadocker-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/"]
+ALLOWED_HOSTS = ["localhost", "localhost:8000","rlc10django.eu-gb.mybluemix.net", "https://romanaleitge-8000.theiadocker-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/"]
 
 
 # Application definition
